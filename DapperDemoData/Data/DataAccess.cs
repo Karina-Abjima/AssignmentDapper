bc23e1eb0496d1cae1d3a4f5bc7771fa5dab6364
Karina-Abjima <karina.gwalani@abjima.com>
2023-05-30 18:16:32 +0530

    .

---
+++ DapperDemoData/Data/DataAccess.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperDemoData.Data
+{
+    public class DataAccess
+    {
+        private readonly IConfiguration
+    }
+}
